5c59a16efe8625d215d4ac16419c93ebbe66656b
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-20 23:14:39 +0300

    FEAT: create and save photo

---
+++ src/controllers/photos.js
@@ -0,0 +1,26 @@
+const Photo = require("../models/photo");
+
+// create a new photo
+exports.create = (req, res) => {
+  // create a new photo object
+  const photo = new Photo({
+    albumId: req.body.albumId,
+    photoTitle: req.body.photoTitle,
+    imageUrl: req.body.imageUrl,
+  });
+  // save the photo
+  photo
+    .save()
+    .then((data) => {
+      res.status(201).send({
+        message: "Photo created successfully!",
+        data,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message || "Some error occurred while creating the photo.",
+      });
+    });
+};
