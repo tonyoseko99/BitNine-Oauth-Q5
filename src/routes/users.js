fdc426729026b129cfa7f9d0b7a4da207b06e974
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-20 23:26:21 +0300

    FEAT: add a users router

---
+++ src/routes/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/users");
+
+router.post("/", userController.create);
+router.get("/", userController.findAll);
+router.get("/:userId", userController.findOne);
+router.put("/:userId", userController.update);
+router.delete("/:userId", userController.delete);
+
+module.exports = router;
