0f1a4497f2556301db8f2e1f74de1c316712e898
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-21 00:19:12 +0300

    use middleware to set up the /api routes

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const mongoose = require("mongoose");
 const usersRouter = require("./src/routes/users");
 const albumsRouter = require("./src/routes/albums");
 const photosRouter = require("./src/routes/photos");
-const port = 4000;
+
+app.use(bodyParser.json());
+app.use("/api/users", usersRouter);
+app.use("/api/albums", albumsRouter);
+app.use("/api/photos", photosRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
