0ed0766fe51d176089edbf4702b26098dab3aa16
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-20 09:53:58 +0300

    FEAT: create the mongoDb instance and export it

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+// create a dbUrl variable and assign a mongodb connection string
+const dbUrl = "mongodb://localhost:27017/galleria-db";
+
+// export the dbUrl variable
+module.exports = { dbUrl };
