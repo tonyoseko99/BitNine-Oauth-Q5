cb653f97dfb138fb70e94fa78505080dfff387b1
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-21 00:16:38 +0300

    import the routers into the primary index.js file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const usersRouter = require("./src/routes/users");
+const albumsRouter = require("./src/routes/albums");
+const photosRouter = require("./src/routes/photos");
 const port = 4000;
 
 app.get("/", (req, res) => {
