10a522ebdfda9941b19d94cce53048d6c0fd1d3d
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-20 14:49:40 +0300

    FEAT: create a new User in the users controller

---
+++ src/controllers/users.js
@@ -0,0 +1,27 @@
+// users controller
+const User = require("../models/user");
+
+// create a new user
+exports.create = (req, res) => {
+  // create a new user object
+  const user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+  });
+  // save the new user
+  user
+    .save()
+    .then((data) => {
+      res.status(200).send({
+        message: "User created successfully",
+        data,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message || "some error occurred while creating the user.",
+      });
+    });
+};
