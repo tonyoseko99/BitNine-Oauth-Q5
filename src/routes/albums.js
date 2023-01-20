dcf719682b7fa8bd49266423de69f0010453ffe4
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-21 00:00:52 +0300

    import the albums validator into the albums route and setup validate middleware

--- src/routes/albums.js
+++ src/routes/albums.js
@@ -1,11 +1,18 @@
 const express = require(express);
 const router = express.Router();
 const albumController = require("../controllers/albums");
+const { validateAlbum } = require("../validations/albums");
+
+const validate = (req, res, next) => {
+  const { error } = validateAlbum(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  next();
+};
 
 router.put("/", albumController.create);
 router.get("/", albumController.findAll);
-router.get('/:albumId', albumController.findOne);
-router.put('/:albumId', albumController.update);
-router.delete('/:albumId', albumController.delete);
+router.get("/:albumId", albumController.findOne);
+router.put("/:albumId", albumController.update);
+router.delete("/:albumId", albumController.delete);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users");
+const { validateUser } = require("../validations/users");
+
+// validate middleware
+const validate = (req, res, next) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  next();
+};
 
 router.post("/", userController.create);
 router.get("/", userController.findAll);
