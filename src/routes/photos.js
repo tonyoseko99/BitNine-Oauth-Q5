901722876e7caae0ab45ac4dfdab1b58e1ca8a8c
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-21 00:01:37 +0300

    import the photo validator into the photos route and setup validate middleware

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const photoController = require("../controllers/photos");
+const { validatePhoto } = require("../validations/photos");
+
+const validate = (req, res, next) => {
+  const { error } = validatePhoto(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  next();
+};
 
 router.post("/", photoController.create);
 router.get("/", photoController.findAll);
