46a2c17a2a93e5a34d089bbcadece5f3aac5d2de
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-24 03:37:50 +0300

    create the signup route

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -4,10 +4,33 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 // user signup
-router.post("/register", async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    } catch (error) {
-        
+router.post("/signup", async (req, res) => {
+  try {
+    // get user input
+    const { name, username, email, password } = req.body;
+    // check if user already exist
+    const oldUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
     }
+    // hash user password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // create user
+    const user = await User.create({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
+    // save user to database
+    await user.save();
+    // send response indicating user creation
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
+
+module.exports = router;
