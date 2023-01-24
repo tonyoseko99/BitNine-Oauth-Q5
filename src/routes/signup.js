0b94094537be8e0e7a68ac25ffb15e65794c318c
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-24 03:49:29 +0300

    add a token

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 // user signup
@@ -28,6 +29,10 @@ router.post("/signup", async (req, res) => {
     await user.save();
     // send response indicating user creation
     res.status(201).json({ message: "User created successfully" });
+    // jwt token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h", // expires in 1 hour
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
