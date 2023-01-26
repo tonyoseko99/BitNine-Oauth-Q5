69907a521c8f0ceaaf116660dd13273cf55c9d16
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-27 02:38:56 +0300

    refactor db connection

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ app.use("/api/albums", albumsRouter);
 app.use("/api/photos", photosRouter);
 
 // connect to mongodb
-mongoose.connect(process.env.PORT, () => {
-  console.log("Mongo connected");
-});
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
+// start the server
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
